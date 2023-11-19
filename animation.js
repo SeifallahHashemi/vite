a7e42ddeb2edf3beef0f34b41949bb2afc586a84
Sepehr Hashemi <sepehrhashemimamaquani1996@gmail.com>
2023-11-19 18:58:35 +0330

    image trail animation

--- animation.js
+++ animation.js
@@ -1,43 +1,85 @@
 import "./main.scss";
-import Lenis from "@studio-freight/lenis";
-import SplitType from "split-type";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
 
-const images = gsap.utils.toArray("img");
-const text = gsap.utils.toArray(".st0");
 
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".imgContainer",
-    start: "top bottom",
-    end: "bottom center",
-    scrub: 4,
-  },
-});
+// Trailing Images
+const hero = document.querySelector('.hero'),
+heroTitle = document.querySelector('.hero-title > .ofh > h1');
 
-images.forEach((image) => {
-  gsap.to(image, {
-    yPercent: -100 * image.dataset.speed,
-    ease: "none",
-    scrollTrigger: {
-      scrub: image.dataset.speed,
-    },
-  });
-});
+const settings = {
+  isEnabled: false,
+  count: 1,
+  time: 50,
+}
+const images = [
+  './images/hero_ (1).jpeg',
+  './images/hero_ (1).jpg',
+  './images/hero_ (1).png',
+  './images/hero_ (2).jpeg',
+  './images/hero_ (2).jpg',
+  './images/hero_ (2).png',
+  './images/hero_ (3).jpeg',
+  './images/hero_ (3).jpg',
+  './images/hero_ (4).jpeg',
+  './images/hero_ (4).jpg',
+  './images/hero_ (5).jpg',
+  './images/hero_ (6).jpg',
+]
 
-tl.to(text, {
-  strokeDashoffset: 0,
-}).to(text, {
-  strokeDashoffset: 470,
-});
+const loadImages = () => {
+  for (let i = 0; i < images.length; i++) {
+    let link = document.createElement('link');
+    link.rel = 'preload'
+    link.as = 'image';
+    link.href = images[i],
+    document.head.appendChild(link)
+  }
+}
+const calcIndex = (length) => {
+  settings.count++;
+  if (settings.count == length) settings.count = 0
+  return settings.count
+}
+const animateImage = (event) => {
+  const image = document.createElement('img')
+  const imageSize = 20
+  const countIndex = calcIndex(images.length)
 
-const lenis = new Lenis();
+  image.classList.add('hero_media')
+  image.setAttribute('src', images[countIndex])
+  image.style.width = `${imageSize}rem`
+  image.style.height = `${imageSize}rem`
+  image.style.top = event.pageY - (imageSize * 10) / 2 + 'px';
+  image.style.left = event.pageX - (imageSize * 10) / 2 + 'px';
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+  hero.appendChild(image);
+
+  const randomDeg = Math.floor(Math.random() * 15);
+  window.setTimeout(() => {
+    image.style.transform = `scale(1)`
+    image.style.transform = `rotate(${randomDeg}deg)`
+  }, 50)
+
+  window.setTimeout(() => {
+    image.style.opacity = 0
+    image.style.filter = `blur(10px)`
+    image.style.transform = `scale(0.25)`
+  }, 1500)
+
+  window.setTimeout(() => {
+    hero.removeChild(image)
+  }, 2500)
 }
 
-requestAnimationFrame(raf);
+window.addEventListener('mousemove', (event) => {
+  if (!settings.isEnabled) {
+    settings.isEnabled = true;
+    setTimeout(() => {
+      settings.isEnabled = false
+    }, settings.time)
+    animateImage(event)
+  }
+})
+
+window.onload = () => {
+  loadImages()
+}
