fd22844542c795f09934d214e2bc048d696fd484
Sepehr Hashemi <sepehrhashemimamaquani1996@gmail.com>
2023-12-29 18:19:21 +0330

    finished

---
+++ animation.js
@@ -0,0 +1,13 @@
+const wrapper = document.querySelector('.cards');
+const cards = wrapper.querySelectorAll('.card');
+
+wrapper.addEventListener('mousemove', (event) => {
+    cards.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        card.style.setProperty('--xPos', `${x}px`);
+        card.style.setProperty('--yPos', `${y}px`);
+    })
+})
