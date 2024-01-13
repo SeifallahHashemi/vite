10cedd129b54460a21b4a1cd9f93122c0728e7e6
SEPEHR HASHEMI <sepehrhashemimamaquani1996@gmail.com>
2024-01-13 12:09:58 +0330

    infinite_horizontal_scroll

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /* JS */
 /* Styles */
 import "./main.scss"
+
+const scrollerWrapper = document.querySelectorAll('.scroller');
+scrollerWrapper.forEach(scroller => {
+    
+    const scrollers = Array.from(scroller.children);
+    scrollers.forEach(item => {
+        const duplicatedItem = item.cloneNode(true);
+        scroller.appendChild(duplicatedItem);
+    })
+})
