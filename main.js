e9ffabb22e6cc7381b6a94efaddf033887ec73e6
SEPEHR HASHEMI <sepehrhashemimamaquani1996@gmail.com>
2024-01-15 17:19:26 +0330

    tubelight-lamp

--- main.js
+++ main.js
@@ -2,13 +2,19 @@
 /* Styles */
 import "./main.scss"
 import SplitType from "split-type";
-import gsap from "gsap";
+import gsap, {} from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const myText = new SplitType('#my-text')
 
-gsap.to('.char', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.2,
-    duration: .1
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".container",
+        start: "-260px top",
+        end: "300px center",
+        scrub: 1,
+        // markers: true,
+    }
 })
+tl.to(".lampcontainer", { scale: 0.6}, 0);
